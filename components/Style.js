1dd066f5ae640d020170c627096cd6fe91ea7c9f
devLeSilverio <leticia.silverio09@gmail.com>
2021-09-23 10:48:29 -0300

    trying to put an example about how could use useEffect

---
+++ components/Style.js
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  separator:{
+    marginVertical:2,
+  },
+  titleMain:{
+    fontSize:30,
+    color:'black',
+    fontWeight:'bold',
+  },
+  titleEnd:{
+    marginTop:20,
+    marginBottom:10,
+    fontSize:20,
+    color:'black',
+    fontWeight:'bold',
+    borderWidth:2,
+    borderRadius:4,
+  },
+  text:{
+    fontSize:20,
+    color:'black',
+    fontWeight:'bold',
+  },
+  photo1:{
+    width:150,
+    height:150,
+    marginLeft:80,
+  },
+  photo2:{
+    width:200,
+    height:150,
+    marginLeft:60,
+    marginBottom:10,
+  },
+  container:{
+    width:'90%',
+  }
+
+
+})
